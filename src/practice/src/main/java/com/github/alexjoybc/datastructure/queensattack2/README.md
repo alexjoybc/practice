b871b7375df7014c2d1a6f3d0c0cf7db9690e53a
alex <alexandre.joyeux@nttdata.com>
2021-03-07 18:09:01 -0800

    30 point hacker rank solution

--- src/practice/src/main/java/com/github/alexjoybc/datastructure/queensattack2/README.md
+++ src/practice/src/main/java/com/github/alexjoybc/datastructure/queensattack2/README.md
@@ -4,3 +4,12 @@
 
 # Time complexity
 
+
+n is size of the input array
+
+at maximum, we would loop on every item of the array once
+
+T = an + b: an is the fastest growing term as b does not grow, after taking the coefficient out a we get O(n)
+
+the function as a linear time complexity O(n)
+
--- src/practice/src/main/java/com/github/alexjoybc/datastructure/queensattack2/Solution.java
+++ src/practice/src/main/java/com/github/alexjoybc/datastructure/queensattack2/Solution.java
@@ -1,11 +1,12 @@
 package com.github.alexjoybc.datastructure.queensattack2;
 
+import java.util.Arrays;
+
 public class Solution {
 
     // Complete the queensAttack function below.
     static int queensAttack(int n, int k, int r_q, int c_q, int[][] obstacles) {
 
-        int moves = 0;
 
         int maxMovesN = n - r_q;
         int maxMovesNE = Math.min(n - r_q, n - c_q);
@@ -16,22 +17,66 @@ public class Solution {
         int maxMovesW = c_q - 1;
         int maxMovesNW = Math.min(n - r_q, c_q - 1);
 
-        for(int i = 0; i < obstacles.length; i++) {
 
-            // is the ostacle south
-            // 3 / 4
-            // 4 - 3
-            if(obstacles[i][1] == c_q && r_q - obstacles[i][1] - 1 < maxMovesS) maxMovesS =  r_q - obstacles[i][1] - 1;
 
+        // adjust max moves based on the obstacle position
+        for (int i = 0; i < obstacles.length; i++) {
+
+            // adjust north and south
+            if (obstacles[i][1] == c_q) {
+
+                // adjust north
+                if (obstacles[i][0] > r_q && obstacles[i][0] - r_q - 1 < maxMovesN)
+                    maxMovesN = obstacles[i][0] - r_q - 1;
+
+                // adjust south
+                if (obstacles[i][0] < r_q && r_q - obstacles[i][0] - 1 < maxMovesS)
+                    maxMovesS = r_q - obstacles[i][0] - 1;
+
+            }
+
+
+            // adjust east and west
+            if (obstacles[i][0] == r_q) {
+
+                // adjust east
+                if (obstacles[i][1] > c_q && obstacles[i][1] - c_q - 1 < maxMovesE)
+                    maxMovesE = obstacles[i][1] - c_q - 1;
+
+                // adjust west
+                if (obstacles[i][1] < c_q && c_q - obstacles[i][1] - 1 < maxMovesW)
+                    maxMovesW = c_q - obstacles[i][1] - 1;
+
+            }
+
+            // adjust diagonals
+            if (Math.abs(obstacles[i][0] - r_q) == Math.abs(obstacles[i][1] - c_q)) {
+
+                // adjust north east
+                if (obstacles[i][0] > r_q && obstacles[i][1] > c_q && obstacles[i][0] - r_q - 1 < maxMovesNE)
+                    maxMovesNE = obstacles[i][0] - r_q - 1;
+
+                // adjust north west
+                if (obstacles[i][0] > r_q && obstacles[i][1] < c_q && obstacles[i][0] - r_q - 1 < maxMovesNW)
+                    maxMovesNW = obstacles[i][0] - r_q - 1;
+
+                // adjust south east
+                if (obstacles[i][0] < r_q && obstacles[i][1] > c_q && r_q - obstacles[i][0] - 1 < maxMovesSE)
+                    maxMovesSE = r_q - obstacles[i][0] - 1;
+
+                // adjust south west
+                if (obstacles[i][0] < r_q && obstacles[i][1] < c_q && r_q - obstacles[i][0] - 1 < maxMovesSW)
+                    maxMovesSW = r_q - obstacles[i][0] - 1;
+
+
+            }
 
 
         }
-
 
         return maxMovesN + maxMovesNE + maxMovesE + maxMovesSE + maxMovesS + maxMovesW + maxMovesSW + maxMovesNW;
 
 
     }
-
 
 }
